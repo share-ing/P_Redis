1f415a2f8b159d9fe8bc2520dcd9608866e9601f
夏顺意 <1207049029@qq.com>
2020-05-09 17:30:11 +0800

    新建项目

---
+++ P_Redis/Func/RedisHelper.cs
@@ -0,0 +1,51 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedisDemo.Func
+{
+    public class RedisHelper
+    {
+        private static ConnectionMultiplexer Redis { get; set; }
+        private static IDatabase Db { get; set; }
+        static RedisHelper()
+        {
+            Redis = ConnectionMultiplexer.Connect("127.0.0.1:6379");
+            Db = Redis.GetDatabase();
+        }
+
+        /// <summary>
+        /// 增加/修改
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <param name="value">默认1分钟</param>
+        /// <returns></returns>
+        public static bool Set(string key, string value, double minute = 1)
+        {
+            return Db.StringSet(key, value, TimeSpan.FromMinutes(minute));
+        }
+
+        /// <summary>
+        /// 查询
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static string Get(string key)
+        {
+            return Db.StringGet(key);
+        }
+
+        /// <summary>
+        /// 删除
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static bool Delete(string key)
+        {
+            return Db.KeyDelete(key);
+        }
+    }
+}
